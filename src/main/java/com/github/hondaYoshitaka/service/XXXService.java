c9dae68c981c63e066edfd10f60b2b24121cfe0d
hondaYoshitaka <yhonda1001@gmail.com>
2019-02-27 19:17:20 +0900

    :recycle: add readonly Transaction

--- src/main/java/com/github/hondaYoshitaka/service/XXXService.java
+++ src/main/java/com/github/hondaYoshitaka/service/XXXService.java
@@ -7,12 +7,15 @@ import com.github.hondaYoshitaka.model.response.XXXPostResponseDto;
 import com.github.hondaYoshitaka.model.response.XXXsFetchResponseDto;
 import com.github.hondaYoshitaka.model.response.XXXsFetchResponseDto.XXXDto;
 import com.github.hondaYoshitaka.repository.XXXRepository;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
 
 @Service
+@Transactional(readOnly = true)
 public class XXXService {
 
     private final XXXRepository xxxRepository;
@@ -21,6 +24,10 @@ public class XXXService {
         this.xxxRepository = xxxRepository;
     }
 
+    /**
+     * @param id
+     * @return
+     */
     public XXXFetchResponseDto fetchOne(final Long id) {
         final var entity = xxxRepository.findOne(id);
 
@@ -34,12 +41,20 @@ public class XXXService {
                                   .build();
     }
 
+    /**
+     * @param categoryId
+     * @param price
+     * @param pageable
+     * @return
+     */
     public XXXsFetchResponseDto fetchAll(final Long categoryId,
                                          final Integer price,
                                          final Pageable pageable) {
+        final RowBounds rowBounds = new RowBounds(Math.toIntExact(pageable.getOffset()),
+                                                  pageable.getPageSize());
         final var entities = xxxRepository.findAllByCategory(categoryId,
                                                              price,
-                                                             pageable);
+                                                             rowBounds);
         final var dtos = entities.stream()
                                  .map(XXXService::mapXxxDto)
                                  .collect(Collectors.toList());
@@ -49,6 +64,13 @@ public class XXXService {
                                    .build();
     }
 
+    /**
+     * @param name
+     * @param categoryId
+     * @param price
+     * @return
+     */
+    @Transactional
     public XXXPostResponseDto postOne(final String name,
                                       final Long categoryId,
                                       final Integer price) {
@@ -67,6 +89,10 @@ public class XXXService {
                                  .build();
     }
 
+    /**
+     * @param entity
+     * @return
+     */
     private static XXXDto mapXxxDto(final XXX entity) {
         return XXXDto.builder()
                      .id(entity.getId())
