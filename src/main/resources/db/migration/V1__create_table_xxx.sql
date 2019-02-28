6a84506e98c7da5b98e9cd272efc50e9f39d00f4
hondaYoshitaka <yhonda1001@gmail.com>
2019-02-28 19:42:04 +0900

    :wrench: enabled pk-column auto increment

--- src/main/resources/db/migration/V1__create_table_xxx.sql
+++ src/main/resources/db/migration/V1__create_table_xxx.sql
@@ -1,5 +1,5 @@
 CREATE TABLE xxx (
-  id          BIGSERIAL     PRIMARY KEY,
+  id          SERIAL        PRIMARY KEY,
   name        VARCHAR(255)  NOT NULL,
   category_id BIGSERIAL     NOT NULL,
   price       INTEGER       NOT NULL,
