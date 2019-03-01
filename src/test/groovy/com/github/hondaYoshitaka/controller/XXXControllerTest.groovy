efea97f1a21db2b84da8047f48a9fe8228f958ef
hondaYoshitaka <yhonda1001@gmail.com>
2019-03-01 10:05:06 +0900

    :rotating_light: add controller tests

--- src/test/groovy/com/github/hondaYoshitaka/controller/XXXControllerTest.groovy
+++ src/test/groovy/com/github/hondaYoshitaka/controller/XXXControllerTest.groovy
@@ -3,6 +3,7 @@ package com.github.hondaYoshitaka.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.hondaYoshitaka.model.response.XXXFetchResponseDto
+import com.github.hondaYoshitaka.model.response.XXXsFetchResponseDto
 import com.github.hondaYoshitaka.service.XXXService
 import org.mockito.InjectMocks
 import org.mockito.Mockito
@@ -10,33 +11,34 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.domain.Pageable
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.util.LinkedMultiValueMap
 import spock.lang.Specification
 import spock.lang.Unroll
+
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Unroll
 class XXXControllerTest extends Specification {
-    @Autowired
-    MockMvc mockMvc
-
-    @Autowired
-    ObjectMapper objectMapper
-
     @InjectMocks
     XXXController xxxController
 
     @MockBean
     XXXService xxxService
 
+    @Autowired
+    MockMvc mockMvc
+
+    @Autowired
+    ObjectMapper objectMapper
+
     def "xxx１件取得_idに対応するエンティティが存在する場合、レスポンスのフィールド値が取得できること"() {
         given:
-        def id = 0L
-
         def responseDto = XXXFetchResponseDto.builder().id(999L).name('xxx name').price(888).build()
         Mockito.when(xxxService.fetchOne(id)).thenReturn(responseDto)
 
@@ -51,13 +53,15 @@ class XXXControllerTest extends Specification {
         assert contents.path('id').asLong() == 999
         assert contents.path('name').asText() == 'xxx name'
         assert contents.path('price').asInt() == 888
+
+        where:
+        id || _
+        0L || _
     }
 
-    def "xxx１件取得_idに対応するエンティティが存在しない場合、レスポンスnullが取得できること"() {
+    def "xxx１件取得_idに対応するエンティティが存在しすべての項目が空の場合、レスポンスのフィールド値が取得できること"() {
         given:
-        def id = 0L
-
-        def responseDto = null
+        def responseDto = XXXFetchResponseDto.builder().build()
         Mockito.when(xxxService.fetchOne(id)).thenReturn(responseDto)
 
         when:
@@ -68,7 +72,13 @@ class XXXControllerTest extends Specification {
         then:
         def contents = objectMapper.readTree(actual.response.contentAsByteArray)
 
-        assert contents == null
+        assert contents.path('id').isNull()
+        assert contents.path('name').isNull()
+        assert contents.path('price').isNull()
+
+        where:
+        id || _
+        0L || _
     }
 
     def "xxx１件取得_idの型が不一致の場合、バリデーションエラーになること"() {
@@ -90,7 +100,52 @@ class XXXControllerTest extends Specification {
         'aaaa' || 'id'  | 'typeMismatch'
     }
 
-    @Unroll
+    def "xxx検索_リクエストが[#name, #categoryId, #price]の場合、レスポンスのフィールド値が取得できること"() {
+        given:
+        def params = new LinkedMultiValueMap<String, String>()
+        name?.with { params.add('name', it) }
+        categoryId?.with { params.add('categoryId', it) }
+        price?.with { params.add('price', it) }
+
+        def xxxs = [XXXsFetchResponseDto.XXXDto.builder().id(1).name('name 1').price(1).build()]
+        def responseDto = XXXsFetchResponseDto.builder().xxxs(xxxs).build()
+
+        Mockito.when(xxxService.fetchAll(Mockito.eq(categoryId.toLong()), Mockito.eq(price.toInteger()), Mockito.any(Pageable)))
+                .thenReturn(responseDto)
+
+        when:
+        def actual = mockMvc.perform(get("/api/xxx").params(params))
+                .andExpect(status().isOk())
+                .andReturn()
+
+        then:
+        def contents = objectMapper.readTree(actual.response.contentAsByteArray)
+
+        def errors = contents.path('xxxs').asList()
+        assert errors.size() == 1
+        assert errors[0].path('id').asLong() == 1L
+        assert errors[0].path('name').asText() == 'name 1'
+        assert errors[0].path('price').asInt() == 1
+
+        where:
+        name      | categoryId          | price     || _
+        // name
+        ''        | '2'                 | '2'       || _
+        'a'       | '2'                 | '2'       || _
+        'a' * 255 | '2'                 | '2'       || _
+        // category id
+        'name'    | '' + Long.MIN_VALUE | '2'       || _
+        'name'    | '-1'                | '2'       || _
+        'name'    | '0'                 | '2'       || _
+        'name'    | '1'                 | '2'       || _
+        'name'    | '' + Long.MAX_VALUE | '2'       || _
+        // price
+        'name'    | '2'                 | '0'       || _
+        'name'    | '2'                 | '1'       || _
+        'name'    | '2'                 | '999999'  || _
+        'name'    | '2'                 | '1000000' || _
+    }
+
     def "xxx検索_リクエストが[#name, #categoryId, #price]の場合、バリデーションエラーとなること"() {
         given:
         def params = new LinkedMultiValueMap<String, String>()
@@ -112,9 +167,16 @@ class XXXControllerTest extends Specification {
         assert errors[0].path('code').asText() == code
 
         where:
-        name   | categoryId | price || field        | code
-        null   | '1'        | '2'   || 'name'       | 'NotNull'
-        'name' | null       | '2'   || 'categoryId' | 'NotNull'
-        'name' | '1'        | null  || 'price'      | 'NotNull'
+        name      | categoryId | price     || field        | code
+        null      | '1'        | '2'       || 'name'       | 'NotNull'
+        'a' * 256 | '1'        | '2'       || 'name'       | 'Length'
+        'name'    | null       | '2'       || 'categoryId' | 'NotNull'
+        'name'    | ''         | '2'       || 'categoryId' | 'NotNull'
+        'name'    | '1'        | null      || 'price'      | 'NotNull'
+        'name'    | '1'        | ''        || 'price'      | 'NotNull'
+        'name'    | '1'        | '-2'      || 'price'      | 'Range'
+        'name'    | '1'        | '-1'      || 'price'      | 'Range'
+        'name'    | '1'        | '1000001' || 'price'      | 'Range'
+        'name'    | '1'        | '1000002' || 'price'      | 'Range'
     }
 }
