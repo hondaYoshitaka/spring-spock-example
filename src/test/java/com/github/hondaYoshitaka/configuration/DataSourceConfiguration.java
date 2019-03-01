b0f571ee7622d3b0df6ef347e8238eeb09e5438e
hondaYoshitaka <yhonda1001@gmail.com>
2019-03-01 15:24:14 +0900

    :recycle: rename datasource configuration

--- src/test/java/com/github/hondaYoshitaka/configuration/DataSourceConfiguration.java
+++ src/test/java/com/github/hondaYoshitaka/configuration/DataSourceConfiguration.java
@@ -13,6 +13,12 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfiguration {
 
+    /**
+     * https://qiita.com/kazuki43zoo/items/d9182c8ddb2b754f150f#spring%E3%81%AE%E3%83%88%E3%83%A9%E3%83%B3%E3%82%B6%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%B3%E7%AE%A1%E7%90%86%E4%B8%8B%E3%81%B8%E3%81%AE%E5%8F%82%E5%8A%A0
+     *
+     * @param properties
+     * @return
+     */
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public DataSource dataSource(final DataSourceProperties properties) {
