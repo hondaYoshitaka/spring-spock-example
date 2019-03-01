a818d7153e06def3e978217d81b06a3ae560eb5d
hondaYoshitaka <yhonda1001@gmail.com>
2019-03-01 15:24:23 +0900

    :books: add Javadoc

--- src/test/java/com/github/hondaYoshitaka/configuration/DataSourceConfiguration.java
+++ src/test/java/com/github/hondaYoshitaka/configuration/DataSourceConfiguration.java
@@ -10,15 +10,17 @@ import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
 import javax.sql.DataSource;
 
+/**
+ * Spring/3rd Partyライブラリ間のトランザクションを繋ぐためのDataSourceを提供するconfigurationクラス.
+ * ----------------------------------------------------------------------
+ * - for DbUnit
+ *
+ * @author hondaYoshitaka
+ * @see <a href="https://qiita.com/kazuki43zoo/items/d9182c8ddb2b754f150f">3rdパーティ製のDBアクセスライブラリをSpringのトランザクション管理下に参加させる方法</a>
+ */
 @Configuration
-public class DatabaseConfiguration {
+public class DataSourceConfiguration {
 
-    /**
-     * https://qiita.com/kazuki43zoo/items/d9182c8ddb2b754f150f#spring%E3%81%AE%E3%83%88%E3%83%A9%E3%83%B3%E3%82%B6%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%B3%E7%AE%A1%E7%90%86%E4%B8%8B%E3%81%B8%E3%81%AE%E5%8F%82%E5%8A%A0
-     *
-     * @param properties
-     * @return
-     */
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public DataSource dataSource(final DataSourceProperties properties) {
