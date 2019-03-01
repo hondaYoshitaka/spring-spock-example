f352f3f6a746febd980145391ff36f727bfb0332
hondaYoshitaka <yhonda1001@gmail.com>
2019-03-01 14:44:34 +0900

    :wastebasket: remove un-used param "name"

--- src/main/java/com/github/hondaYoshitaka/model/form/XXXsFetchForm.java
+++ src/main/java/com/github/hondaYoshitaka/model/form/XXXsFetchForm.java
@@ -1,17 +1,12 @@
 package com.github.hondaYoshitaka.model.form;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 public class XXXsFetchForm {
-    @NotNull
-    @Length(max = 255)
-    private String name;
-
     @NotNull
     private Long categoryId;
 
